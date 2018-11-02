407c6ff19e0a9cd83e0a925219871337ed035821
Raphael Katana <roduor41@gmail.com>
2018-11-02 15:14:31 +0300

    inialised an instance  of class. create function to save user info

--- account.py
+++ account.py
@@ -3,3 +3,14 @@ class User:
   Stores user data
   '''
   user_info=[]
+
+  def __init__(self,f_name,l_name,p_number):
+    self.f_name=f_name
+    self.l_name=l_name
+    self.p_number=p_number
+
+  def save_user(self):
+    '''
+    save user info
+    '''
+    User.user_info.append(self)
