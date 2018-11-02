069f2547eda4db05751b3f996ef42f4a11521c13
Raphael Katana <roduor41@gmail.com>
2018-11-02 18:15:28 +0300

    add tearDon method to clear after every test

--- test.py
+++ test.py
@@ -4,10 +4,26 @@ from credentials import Credentials
 
 class TestUser(unittest.TestCase):
   def setUp(self):
-    self.new_user= User('Raphael','Katana','0720132613')
+    self.new_user= User('Raphael','Katana','0720132613') #new user
+    
+    self.new_credentials= Credentials('1123','hjk') #new credentials
 
-  def __init__(self):
-    self.assertEqual(User.f_name,'Raphael')
+  def test_init(self):
+    self.assertEqual(self.new_user.f_name,'Raphael')
+    self.assertEqual(self.new_user.l_name,'Katana')
+    self.assertEqual(self.new_user.p_number,'0720132613')
+
+    #assert test for  credentials
+
+    self.assertEqual(self.new_credentials.acc_name,'1123')
+    self.assertEqual(self.new_credentials.password,'hjk')
+
+  def tearDown(self):
+    user_details=[]
+    user_info=[]
+
+
+
 
 if __name__=='__main__':
   unittest.main()
