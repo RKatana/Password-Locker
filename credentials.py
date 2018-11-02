63530a438941596acd5ff1a84333d5f7538c252a
Raphael Katana <roduor41@gmail.com>
2018-11-02 15:37:17 +0300

    add method to save  user  credentials

--- credentials.py
+++ credentials.py
@@ -4,6 +4,14 @@ class Credentials:
   '''
   class for user credentials
   '''
+  user_details=[]
+
   def __init__(self,acc_name,password):
     self.acc_name=acc_name
     self.password=password
+
+  def save_info(self):
+    '''
+    save user credentials
+    '''
+    Credentials.user_details.append(self)
