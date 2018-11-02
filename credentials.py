f93eff246d76817688c92fdc00e92ab4d5aca2d8
Raphael Katana <roduor41@gmail.com>
2018-11-02 15:32:17 +0300

    created class for credentials and imported class for user info

---
+++ credentials.py
@@ -0,0 +1,9 @@
+import pyperclip
+import User from account
+class Credentials:
+  '''
+  class for user credentials
+  '''
+  def __init__(self,acc_name,password):
+    self.acc_name=acc_name
+    self.password=password
